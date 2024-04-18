83db2b6028517d51b2911b0fc530b5b9232b92b0
diecolgar <diecolgar@gmail.com>
2024-04-18 13:57:59 +0200

    Core Themes optimization

--- neighbourhoods/nb-detail/nb-detail.js
+++ neighbourhoods/nb-detail/nb-detail.js
@@ -1,12 +1,18 @@
 // nb-detail.js
 document.addEventListener('DOMContentLoaded', function() {
   var nbParam = getQueryParam('nb');
+  console.log(nbParam)
   if (nbParam) {
       loadBooths(nbParam); // Carga los booths basados en el parámetro de área
       loadServices(nbParam); // Carga los servicios basados en el parámetro de área
-  } else {
-      console.error('No se especificó el área en la URL.');
-  }
+      updateContentTitle(nbParam); // Actualiza el título y el icono basado en el parámetro de área
+    } else {
+    console.error('No se especificó el área en la URL.');
+    }
+
+    if (nbParam === "th") {
+    loadAuthors(); // Carga la lista de autores si nbParam es igual a "th"
+    }
 });
 
 // Esta función obtiene el valor de un parámetro de la URL por su nombre
@@ -20,6 +26,49 @@ function getQueryParam(param) {
       }
   }
   return null;
+}
+
+function updateContentTitle(nbParam) {
+    var titleIcon = document.getElementById('content-title-icon');
+    var titleText = document.getElementById('content-title-text');
+
+    var iconPath = '';
+    var text = '';
+
+    switch (nbParam) {
+        case 'te':
+            iconPath = 'icon_te.svg';
+            text = 'Technology';
+            break;
+        case 'cs':
+            iconPath = 'icon_cs.svg';
+            text = 'Future of Sustainability';
+            break;
+        case 'cx':
+            iconPath = 'icon_cx.svg';
+            text = 'Customer Engagement';
+            break;
+        case 'pe':
+            iconPath = 'icon_pe.svg';
+            text = 'Strategy, People and Organization';
+            break;
+        case 'op':
+            iconPath = 'icon_op.svg';
+            text = 'Operations';
+            break;
+        case 'ai': // Assuming this is the (Gen)AI Path from the last screenshot
+            iconPath = 'icon_ai.png';
+            text = '(Gen)AI Path';
+            break;
+        case 'th': // Assuming this is the (Gen)AI Path from the last screenshot
+        iconPath = 'icon_th.svg';
+        text = 'Micro-Theater';
+        break;
+    }
+
+    // Actualiza el SRC del icono y el texto
+    titleIcon.style.backgroundImage = 'url(../../assets/neighbourhoods/' + iconPath + ')'; // Asegúrate de que la ruta es correcta
+    titleText.textContent = text;
 }
   
 // Esta función construye el HTML para cada booth y lo agrega al content-container
@@ -48,11 +97,11 @@ function displayBooths(booths) {
         svgPlaceholder.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"> <path d="M9 18L15 12L9 6" stroke="#21BF61" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </svg>';
         boothLink.appendChild(svgPlaceholder);
 
+        container.appendChild(boothLink);
+
         var separator = document.createElement('div');
         separator.className = 'booth-separator';
         container.appendChild(separator);
-
-        container.appendChild(boothLink);
     });
 } 
 
@@ -72,13 +121,69 @@ function displayServices(services) {
       title.className = 'service-title';
       title.textContent = service.title;
       serviceElement.appendChild(title);
+      
+      container.appendChild(serviceElement);
 
       var separator = document.createElement('div');
       separator.className = 'booth-separator';
       container.appendChild(separator);
-      
-      container.appendChild(serviceElement);
   });
+}
+
+// Función para construir el HTML para cada autor y agregarlo al static-content-container
+function displayAuthors(authors) {
+    var container = document.getElementById('static-content-container');
+    authors.forEach(function(author) {
+        var authorElement = document.createElement('div');
+        authorElement.className = 'author-entry';
+
+        // Crear un contenedor para el tiempo y el título
+        var timeTitleContainer = document.createElement('div');
+        timeTitleContainer.className = 'time-title-container';
+
+        var time = document.createElement('div');
+        time.className = 'author-time';
+        time.textContent = author.time;
+        timeTitleContainer.appendChild(time); // Añadir tiempo al contenedor de tiempo y título
+
+        var title = document.createElement('div');
+        title.className = 'author-title';
+        title.textContent = author.title;
+        timeTitleContainer.appendChild(title); // Añadir título al contenedor de tiempo y título
+
+        // Añadir el contenedor de tiempo y título al elemento del autor
+        authorElement.appendChild(timeTitleContainer);
+
+        var description = document.createElement('div');
+        description.className = 'author-description';
+        description.textContent = author.description;
+        authorElement.appendChild(description);
+
+        // Crear un contenedor para la imagen y el nombre del autor
+        var authorInfoContainer = document.createElement('div');
+        authorInfoContainer.className = 'author-info';
+
+        var picture = document.createElement('img');
+        picture.className = 'author-picture';
+        picture.src = '../../assets/talkers/' + author.picture;
+        authorInfoContainer.appendChild(picture); // Añadir imagen al contenedor de información del autor
+
+        var name = document.createElement('div');
+        name.className = 'author-name';
+        name.textContent = author.author;
+        authorInfoContainer.appendChild(name); // Añadir nombre al contenedor de información del autor
+
+        // Añadir el contenedor de información del autor al elemento del autor
+        authorElement.appendChild(authorInfoContainer);
+
+        // Añadir el elemento del autor al contenedor principal
+        container.appendChild(authorElement);
+
+        // Añadir un separador entre elementos del autor
+        var separator = document.createElement('div');
+        separator.className = 'booth-separator';
+        container.appendChild(separator);
+    });
 }
 
 // Función para cargar los booths desde booths-db.json
@@ -104,7 +209,6 @@ function loadBooths(nbParam) {
   xhr.send();
 }
 
-
 // Función para cargar los servicios desde services.json
 function loadServices(nbParam) {
   var xhr = new XMLHttpRequest();
@@ -124,6 +228,24 @@ function loadServices(nbParam) {
       console.error('Error de conexión al intentar cargar el archivo JSON de servicios.');
   };
   xhr.send();
+}
+
+// Función para cargar la lista de autores y mostrarlos en el contenedor
+function loadAuthors() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'booths/talks.json', true); // Asegúrate de que la URL es correcta
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var authorsData = JSON.parse(xhr.responseText);
+            displayAuthors(authorsData.entries);
+        } else {
+            console.error('No se pudo cargar el archivo JSON de autores.');
+        }
+    };
+    xhr.onerror = function() {
+        console.error('Error de conexión al intentar cargar el archivo JSON de autores.');
+    };
+    xhr.send();
 }
   
 // Función para agregar los hitboxes al SVG y envolverlos en un anchor con href que contiene el ID del booth
@@ -146,7 +268,7 @@ function loadHitboxes(booths) {
         svgElement.appendChild(anchor); // Agregar el anchor al SVG
       }
     });
-  }  
+  }
 
 
 // Función para obtener el valor de un parámetro específico de la URL
